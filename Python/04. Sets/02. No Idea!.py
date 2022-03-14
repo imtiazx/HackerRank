6a636bc8f76d0ad7a323b59be6ff9a8ceb1e4835
imtiazx <imtiazx@protonmail.com>
2022-03-14 10:09:43 +0530

    Sets 02

---
+++ Python/04. Sets/02. No Idea!.py
@@ -0,0 +1,14 @@
+n, m =  map(int, input().split())
+
+e = list(map(int, input().strip().split()))
+
+H = set(map(int, input().strip().split()))
+S = set(map(int, input().strip().split()))
+
+count = 0
+for i in e:
+    if i in H:
+        count = count+1
+    if i in S:
+        count = count-1
+print(count)
