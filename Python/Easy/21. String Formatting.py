2447bc8cc0bd696fba7e3c15a3b6294e518301ad
imtiazx <imtiazx@protonmail.com>
2022-04-13 12:16:07 +0530

    remove old files

--- Python/Easy/21. String Formatting.py
+++
@@ -1,8 +0,0 @@
-def print_formatted(number):
-    l  = len(str(bin(number)[2:]))
-    for i in range(1, number + 1):
-        print(str(i).rjust(l) + ' ' + oct(i)[2:].rjust(l) + ' ' + hex(i)[2:].upper().rjust(l) + ' ' + bin(i)[2:].rjust(l))
-
-if __name__ == '__main__':
-    n = int(input())
-    print_formatted(n)
--- Python/Easy/27. Polar Coordinates.py
+++
@@ -1,7 +0,0 @@
-import cmath
-
-num = complex(input())
-z = complex(num)
-
-for i in range(2):
-    print(cmath.polar(z)[i])
